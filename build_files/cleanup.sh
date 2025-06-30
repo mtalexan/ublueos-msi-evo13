428689a5e94333b7a517fa5c5ae45d563fe6800b
Mike <github@trackit.fe80.email>
2025-06-29 23:54:55 -0600

    log cleanup actions

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -2,6 +2,8 @@
 #
 # There are a LOT of undocumented things that need to be cleaned up for bootc to work.
 
+set -exou pipefail
+
 dnf5 clean all
 # this doesn't clean up for some reason
 rm -rf /var/lib/dnf
